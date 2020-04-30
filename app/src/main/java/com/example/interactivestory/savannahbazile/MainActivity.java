03d6916a9b36f5937ca2282ebf7a04e1f1e47f68
savannah0125 <princesssav0125@aol.com>
2020-04-30 18:08:15 -0400

    hello people

--- app/src/main/java/com/example/interactivestory/savannahbazile/MainActivity.java
+++ app/src/main/java/com/example/interactivestory/savannahbazile/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.interactivestory.savannahbazile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText userInput;
+    Button startButton;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.userInput);
+        startButton = findViewById(R.id.startButton);
+        //1.Store our user's name inside of a variable
+        //2.Navigate to a new screen.
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = userInput.getText().toString();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                startStory();
+            }
+        });
+    }
+
+    public void startStory() {
+
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        startActivity(intent);
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/interactivestory/savannahbazile/Orientation.java
@@ -0,0 +1,57 @@
+package com.example.interactivestory.savannahbazile;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+
+public class Orientation extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @SuppressLint("SourceLockedOrientationActivity")
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+            activity.setRequestedOrientation(SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
+    }
+}
+
